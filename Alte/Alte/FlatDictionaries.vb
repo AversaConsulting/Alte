ad59471872449d2e14adda0f8c96931245b5d57e
Nick Denker <nick@aversa.co.uk>
2019-09-25 13:42:14 +0100

    Rejig

--- Alte/Alte/FlatDictionaries.vb
+++
@@ -1,6 +0,0 @@
-﻿Imports Microsoft.WindowsAzure.Storage.Table
-
-Public Class FlatDictionaries
-    Property FullDictionary As Dictionary(Of String, EntityProperty)
-    Property FTSstoredDictionary As Dictionary(Of String, EntityProperty)
-End Class
--- Alte/Alte/FullTextResult.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FullTextResult
-
-    Property ID As String
-    Property Properties As New Dictionary(Of String, Object)
-
-End Class
